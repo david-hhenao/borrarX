4b4acbd7a094240fe59904e6dbf48f866ecd6c6e
David Henao <dhhenaom.10d@gmail.com>
2024-12-10 09:19:35 -0500

    prurba de oatch mal echa

---
+++ pruebaaa.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import pandas as pd
+import requests
+
+# Cargar el texto de 'reviews_booking_limpio'
+url = 'https://raw.githubusercontent.com/Izainea/nlp_ean/refs/heads/main/Datos/Datos%20Crudos/reviews_booking_limpio.csv'
+DF=pd.read_csv(url)
+
+# Tokenizar el texto
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(DF['Comentarios'])
+total_words = len(tokenizer.word_index) + 1
+max_sequence_len = 100
+
+# sequences
+
+sequences=tokenizer.texts_to_sequences(DF['Comentarios'])
+padded = pad_sequences(sequences, maxlen=max_sequence_len, padding='post', truncating='post')
+
+padded.shape
